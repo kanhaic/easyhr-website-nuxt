754c8b0d12537a3c185724ee2eefe606f6b678b0
Kanhai Chhugani <kanhai@chhugani.org>
2024-10-28 11:45:29 +0530

    fixed thank you page

---
+++ pages/resources.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Resources</h1>
+    <p>
+        Here you can find all the resources you need to know about our products.
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
