0c72f725869e8000bb5f9d25bcd7fc7c8c332b17
Kanhai Chhugani <kanhai@chhugani.org>
2024-10-30 18:44:10 +0530

    fixed company menu

--- components/Header.vue
+++ components/Header.vue
@@ -297,23 +297,47 @@
               leave-to-class="opacity-0 translate-y-1"
             >
               <PopoverPanel
-                class="absolute -left-8 top-full z-10 mt-3 w-96 rounded-3xl bg-white p-4 shadow-lg ring-1 ring-gray-900/5"
+                class="absolute right-0 sm:-right-8 top-full z-10 mt-3 w-[480px] rounded-3xl bg-white p-4 shadow-lg ring-1 ring-gray-900/5"
               >
-                <div
-                  v-for="item in company"
-                  :key="item.name"
-                  class="relative rounded-lg p-4 hover:bg-gray-50"
-                >
-                  <a
-                    :href="item.href"
-                    class="block text-sm font-semibold leading-6 text-gray-900"
+                <div class="relative">
+                  <div
+                    v-for="item in company"
+                    :key="item.name"
+                    class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50"
                   >
-                    {{ item.name }}
-                    <span class="absolute inset-0" />
-                  </a>
-                  <p class="mt-1 text-sm leading-6 text-gray-600">
-                    {{ item.description }}
-                  </p>
+                    <div
+                      class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg"
+                      :class="item.iconBgColor"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                        :class="item.iconColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          :d="item.iconPath"
+                        />
+                      </svg>
+                    </div>
+                    <div>
+                      <a
+                        :href="item.href"
+                        class="block text-sm font-semibold leading-6 text-gray-900"
+                      >
+                        {{ item.name }}
+                        <span class="absolute inset-0" />
+                      </a>
+                      <p class="mt-1 text-sm leading-6 text-gray-600">
+                        {{ item.description }}
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </PopoverPanel>
             </transition>
@@ -661,21 +685,27 @@ const company = [
   {
     name: "About us",
     href: "/company/about",
-    description:
-      "Learn more about our company values and mission to empower others",
+    description: "Learn more about our company values and mission to empower others",
+    iconBgColor: "bg-blue-100",
+    iconColor: "text-blue-600",
+    iconPath: "M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
   },
   {
     name: "Careers",
     href: "/careers",
-    description:
-      "Looking for you next career opportunity? See all of our open positions",
+    description: "Looking for you next career opportunity? See all of our open positions",
+    iconBgColor: "bg-green-100",
+    iconColor: "text-green-600",
+    iconPath: "M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
   },
   {
     name: "Contact us",
     href: "/company/contact",
-    description:
-      "Get in touch with our dedicated support team or reach out on our community forums",
-  },
+    description: "Get in touch with our dedicated support team or reach out on our community forums",
+    iconBgColor: "bg-purple-100",
+    iconColor: "text-purple-600",
+    iconPath: "M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+  }
 ];
 
 const mobileMenuOpen = ref(false);
