686050a6b5b929ba0b9359be242d544a855c0ad4
Kanhai Chhugani <kanhai@chhugani.org>
2024-11-11 23:30:09 +0530

    added small chat script

--- plugins/small-chat.client.ts
+++ plugins/small-chat.client.ts
@@ -1,6 +1,9 @@
 export default defineNuxtPlugin(() => {
-  const script = document.createElement('script')
-  script.src = 'https://embed.small.chat/T179A90J0GVC000RNZ.js'
-  script.async = true
-  document.body.appendChild(script)
+  nextTick(() => {
+    const script = document.createElement("script");
+    script.src = "https://embed.small.chat/T179A90J0GVC000RNZ.js";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+  });
 }) 
