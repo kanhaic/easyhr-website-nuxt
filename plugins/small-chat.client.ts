96e161a0a3c906172bbbca8a94f4c8bb0f04101e
Kanhai Chhugani <kanhai@chhugani.org>
2024-11-12 00:08:35 +0530

    load script after 5 seconds

--- plugins/small-chat.client.ts
+++ plugins/small-chat.client.ts
@@ -1,10 +1,12 @@
 export default defineNuxtPlugin(() => {
-  setTimeout(() => {
-    const script = document.createElement("script");
+  nextTick(() => {  
+    setTimeout(() => {
+      const script = document.createElement("script");
     script.src = "https://embed.small.chat/T179A90J0GVC000RNZ.js";
     script.async = true;
     script.defer = true;
     script.setAttribute("fetchpriority", "low");
     document.body.appendChild(script);
-  }, 2000);
+    }, 5000);
+  });
 });
