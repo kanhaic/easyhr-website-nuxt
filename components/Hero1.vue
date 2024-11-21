162fe750e04f5e9e4eccb12a4bc502480f3a5883
Kanhai Chhugani <kanhai@chhugani.org>
2024-11-22 01:17:01 +0530

    changes to the position of the image

--- components/Hero1.vue
+++ components/Hero1.vue
@@ -1,31 +1,33 @@
 <template>
   <section class="pt-6">
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div class="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
-        <div class="min-h-[calc(100vh-4rem)] lg:min-h-fit flex flex-col justify-center">
+      <div class="grid items-center grid-cols-1 gap-12 lg:grid-cols-2 min-h-[calc(100vh-128px)] lg:min-h-0">
+        <div class="order-2 lg:order-1">
           <p
-            class="text-xl sm:text-base font-semibold tracking-wider text-blue-600 uppercase"
+            class="text-base font-semibold tracking-wider text-blue-600 uppercase"
           >
             {{ topTag }}
           </p>
-          <h1 class="mt-4 text-5xl font-semibold text-black lg:mt-8 xl:text-6xl">
+          <h1
+            class="mt-4 text-4xl font-semibold text-black lg:mt-8 sm:text-5xl xl:text-6xl"
+          >
             {{ title }}
           </h1>
-          <p class="mt-4 text-xl text-black lg:mt-8 sm:text-xl">
+          <p class="mt-4 text-base text-black lg:mt-8 sm:text-xl">
             {{ subtitle }}
           </p>
 
-          <div class="flex flex-col sm:flex-row gap-4 mt-8 lg:mt-16">
+          <div class="flex flex-row gap-4 mt-8 lg:mt-16">
             <a
               v-if="cta1Link"
               :href="cta1Link"
               :title="cta1Title"
-              class="w-full sm:w-auto inline-flex items-center justify-center px-8 py-5 text-lg font-semibold text-black transition-all duration-200 bg-indigo-300 rounded-md hover:bg-indigo-400 focus:bg-indigo-400 sm:px-6 sm:py-4 sm:text-base"
+              class="inline-flex items-center justify-center px-6 py-4 font-semibold text-black transition-all duration-200 bg-indigo-300 rounded-md hover:bg-indigo-400 focus:bg-indigo-400"
               role="button"
             >
               {{ cta1Title }}
               <svg
-                class="w-7 h-7 ml-2 sm:w-6 sm:h-6"
+                class="w-6 h-6 ml-2"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -45,14 +47,14 @@
               v-if="cta2Link"
               :href="cta2Link"
               :title="cta2Title"
-              class="w-full sm:w-auto inline-flex items-center justify-center px-8 py-5 text-lg font-semibold text-indigo-600 transition-all duration-200 bg-transparent border-2 border-indigo-600 rounded-md hover:bg-indigo-600 hover:text-white focus:bg-indigo-600 focus:text-white sm:px-6 sm:py-4 sm:text-base"
+              class="inline-flex items-center justify-center px-6 py-4 font-semibold text-indigo-600 transition-all duration-200 bg-transparent border-2 border-indigo-600 rounded-md hover:bg-indigo-600 hover:text-white focus:bg-indigo-600 focus:text-white"
               role="button"
             >
               {{ cta2Title }}
             </a>
           </div>
 
-          <p v-if="showLogin" class="mt-6 text-lg text-gray-600 sm:text-base sm:mt-5">
+          <p v-if="showLogin" class="mt-5 text-gray-600">
             Already joined us?
             <a
               href="/login"
@@ -63,17 +65,21 @@
           </p>
         </div>
 
-        <div class="justify-center items-center hidden lg:block">
+        <div class="flex justify-center items-center order-1 lg:order-2">
           <NuxtImg
             v-if="heroImageProvider"
             :src="heroImage"
             :alt="title"
             format="webp"
             quality="75"
+            preload
+            priority
             loading="eager"
             fetchpriority="high"
-            sizes="sm:320px md:480px lg:720px"
-            class="rounded-lg w-full h-auto aspect-[4/3]"
+            sizes="(max-width: 640px) 320px, (max-width: 768px) 480px, 720px"
+            width="720"
+            height="480"
+            class="rounded-lg w-full h-auto"
             :provider="heroImageProvider"
           />
           <img
