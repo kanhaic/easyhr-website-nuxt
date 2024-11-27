45cfc5c9006030d5978ff059147c926ddf382f73
Kanhai Chhugani <kanhai@chhugani.org>
2024-11-27 10:45:17 +0530

    removed header tag from logos section

--- components/LogoSection.vue
+++ components/LogoSection.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="py-8 sm:py-16 lg:py-24">
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <h2 class="text-2xl font-bold text-center text-gray-900 mb-8">
+      <div class="text-2xl font-bold text-center text-gray-900 mb-8">
         {{ title }}
-      </h2>
+      </div>
       <div
         class="grid items-center grid-cols-2 gap-10 sm:gap-y-16 sm:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6"
       >
