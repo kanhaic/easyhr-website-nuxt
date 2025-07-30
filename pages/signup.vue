95fa6f8ddbb69a7bd187a64672360813bf82eb93
Kanhai Chhugani <kanhai@chhugani.org>
2025-07-30 15:56:17 +0530

    feat: update placeholder text and add whereheard field to signup form

--- pages/signup.vue
+++ pages/signup.vue
@@ -241,7 +241,7 @@
                     :class="{ 'border-red-500': errors.whereheard }"
                     class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                   >
-                    <option value="" disabled selected>Select</option>
+                    <option value="" disabled selected>Where did you hear about us?</option>
                     <option value="Referral">Referral</option>
                     <option value="LinkedIn">LinkedIn</option>
                     <option value="Twitter">Twitter</option>
@@ -409,6 +409,7 @@ const form = ref({
   browser: "",
   deviceType: "",
   referrer: "",
+  whereheard: "",
 });
 
 const errors = ref({});
